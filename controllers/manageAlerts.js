da7e8acc9ca312b912297fb9cb96a3060ee4afaa
Erik Lanning <cs@eriklanning.com>
2025-04-12 15:04:12 -0700

    Handling

--- controllers/manageAlerts.js
+++ controllers/manageAlerts.js
@@ -2,7 +2,14 @@ let timer_handles = {};
 const bosses = require('../bosses.json');
 
 const createTimer = (channel, boss_name, boss_time) => {
-    let bossAlertTimes = bosses[boss_name].slice(1);
+    const b = bosses[boss_name];
+
+    if (b === undefined) {
+        console.log(`AlertLogger: ${boss_name} not found in bosses.json`);
+        return;
+    }
+
+    let bossAlertTimes = b.slice(1);
 
     bossAlertTimes.forEach(timer => {
         let timerName = boss_name + timer;
@@ -10,11 +17,11 @@ const createTimer = (channel, boss_name, boss_time) => {
             clearTimeout(timer_handles[timerName])
         }
     });
-    
+
     // if timer is set below 5 minutes (the minimum alert time), send alert instantly
-    if (boss_time && boss_time < 5 * 60 * 1000) {    
+    if (boss_time && boss_time < 5 * 60 * 1000) {
         channel.send({
-            content: `${boss_name} is due in ${(boss_time/(60 * 1000)).toFixed(0)} mins!`,
+            content: `${boss_name} is due in ${(boss_time / (60 * 1000)).toFixed(0)} mins!`,
         })
 
         let dateTime = new Date(Date.now());
@@ -23,15 +30,15 @@ const createTimer = (channel, boss_name, boss_time) => {
     // if timer is set for more than 5 mins of time then set it to alert when the boss is due in 5 mins
     else if (boss_time) {
         bossAlertTimes.forEach(alertTime => {
-            timer_handles[boss_name + alertTime] = setTimeout(function() {
+            timer_handles[boss_name + alertTime] = setTimeout(function () {
                 channel.send({
                     content: `${boss_name} is due in ${alertTime} mins!`,
                 })
-    
+
                 let dateTime = new Date(Date.now());
                 console.log(`AlertLogger: ${boss_name} due alert sent in ${channel} at ${dateTime.toString()}`);
             }
-            , boss_time - (alertTime * 60 * 1000));
+                , boss_time - (alertTime * 60 * 1000));
         });
     }
 }
@@ -41,7 +48,14 @@ const clearTimers = () => {
 }
 
 const clearTimer = (boss_name) => {
-    let bossAlertTimes = bosses[boss_name].slice(1);
+    const b = bosses[boss_name];
+
+    if (b === undefined) {
+        console.log(`AlertLogger: ${boss_name} not found in bosses.json`);
+        return;
+    }
+
+    let bossAlertTimes = b.slice(1);
 
     bossAlertTimes.forEach(timer => {
         let timerName = boss_name + timer;
